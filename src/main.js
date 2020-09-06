bf15d5f71edd1239b9eae54da9d545f283e5e34f
byungs2 <byungs2@naver.com>
2020-09-06 21:48:49 +0900

    SignUp page complete without CSS 2020-09-06

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+const storage = window.sessionStorage;
+axios.defaults.headers.common["jwt-auth-token"] = storage.getItem("jwt-auth-token");
 
 new Vue({
   router,
