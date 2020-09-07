67fc1b3ba34005a763a4ec43486841ae4cb10698
pure-teatree <rldms5120@gmail.com>
2020-09-07 11:17:21 +0900

    0907 favoriteController 수정,favoriteRepository 추가

--- src/main/java/controller/FavoritePicturesController.java
+++ src/main/java/controller/FavoritePicturesController.java
@@ -16,48 +16,50 @@ import repositories.PicturesRepository;
 import repositories.UsersRepository;
 import usersDTO.Users;
 
-
 @RestController
 public class FavoritePicturesController {
-	 private final FavoritePicturesRepository favoriteRepository;
-	  private final PicturesRepository pictureRepository;
-	  private final UsersRepository usersRepository;
-	  
-	  FavoritePicturesController(FavoritePicturesRepository favoriteRepository, PicturesRepository pictureRepository, UsersRepository usersRepository) {
-	    this.favoriteRepository = favoriteRepository;
-	    this.pictureRepository = pictureRepository;
-	    this.usersRepository = usersRepository;
-	  }
-	  
-	  //해당하는 유저의 즐겨찾기에 저장
-	  @PostMapping("/favorite/{userEmail}/{pictureNumber}")
-	  FavoritePictures newFavorite(@PathVariable String userEmail, @PathVariable long pictureNumber) {
-		  Users userId = usersRepository.findByUserEmail(userEmail);
-		  Pictures pictureId = pictureRepository.findById(pictureNumber).orElseThrow(null);
-		  FavoritePictures newFavorite = FavoritePictures.builder().pictureId(pictureId).userId(userId).build();
-		  return favoriteRepository.save(newFavorite);
-	  }
-	  
-	  //유저 한 명의 즐겨찾기 전체 보여 주기 
-	  @GetMapping("/favorite/{userEmail}")
-	  public List <Long> favoriteAll(@PathVariable String userEmail){
-		  ArrayList<Long> pictureNumberList = new ArrayList<>();
-		  List <FavoritePictures> favoriteList = favoriteRepository.findByUserId(usersRepository.findByUserEmail(userEmail));
-		  for(FavoritePictures favorite:favoriteList) {
-			  pictureNumberList.add(favorite.getPictureId().getPictureNumber());
-		  }
-		  return pictureNumberList;
-	  }
-	  
-	  //즐겨찾기 삭제
-	  @DeleteMapping("/favorite/{favoritepictureNumber}")
-	  void deleteFavorite(@PathVariable Long favoritepictureNumber) {
-		  FavoritePictures favorite = favoriteRepository.findById(favoritepictureNumber).orElseThrow(()->null);
-		  favoriteRepository.delete(favorite);;
-	  }
-	  
-	  
-	  //확장기능.즐겨찾기 내에서 검색 (태그검색.picture)
-	  //확장기능.카테고리 저장
-	  
+	private final FavoritePicturesRepository favoriteRepository;
+	private final PicturesRepository pictureRepository;
+	private final UsersRepository usersRepository;
+
+	FavoritePicturesController(FavoritePicturesRepository favoriteRepository, PicturesRepository pictureRepository,
+			UsersRepository usersRepository) {
+		this.favoriteRepository = favoriteRepository;
+		this.pictureRepository = pictureRepository;
+		this.usersRepository = usersRepository;
+	}
+
+	// 해당하는 유저의 즐겨찾기에 저장
+	@PostMapping("/favorite/{userEmail}/{pictureNumber}")
+	public FavoritePictures newFavorite(@PathVariable String userEmail, @PathVariable long pictureNumber) {
+		Users userId = usersRepository.findByUserEmail(userEmail);
+		Pictures pictureId = pictureRepository.findById(pictureNumber).orElseThrow(null);
+		FavoritePictures newFavorite = FavoritePictures.builder().pictureId(pictureId).userId(userId).build();
+		return favoriteRepository.save(newFavorite);
+	}
+
+	// 유저 한 명의 즐겨찾기 전체 보여 주기
+	@GetMapping("/favorite/{userNumber}")
+	public List<Long> favoriteAll(@PathVariable long userNumber) {
+		ArrayList<Long> pictureNumberList = new ArrayList<>();
+		List<FavoritePictures> favoriteList = favoriteRepository
+				.findByUserId(usersRepository.findById(userNumber).orElseThrow(() -> null));
+		for (FavoritePictures favorite : favoriteList) {
+			pictureNumberList.add(favorite.getPictureId().getPictureNumber());
+		}
+		return pictureNumberList;
+	}
+
+	// 즐겨찾기 삭제
+	// Picture Number로 Picture 객체 찾고 찾은 Picture 객체로 favoritePicture 객체 찾아서 삭제
+	@DeleteMapping("/favorite/{pictureNumber}")
+	void deleteFavorite(@PathVariable Long pictureNumber) {
+		Pictures pictureId = pictureRepository.findById(pictureNumber).orElseThrow(() -> null);
+		FavoritePictures favorite = favoriteRepository.findByPictureId(pictureId);
+		favoriteRepository.delete(favorite);
+	}
+
+	// 확장기능.즐겨찾기 내에서 검색 (태그검색.picture)
+	// 확장기능.카테고리 저장
+
 }
--- src/main/java/repositories/FavoritePicturesRepository.java
+++ src/main/java/repositories/FavoritePicturesRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import favorite.picturesDTO.FavoritePictures;
+import picturesDTO.Pictures;
 import usersDTO.Users;
 
 @Repository
 public interface FavoritePicturesRepository extends JpaRepository<FavoritePictures, Long> {
 	// 해당유저의 즐겨찾기 리스트
 	public List<FavoritePictures> findByUserId(Users userId);
+
+	// pictureId에 해당하는 FavoritePictures 객체
+	public FavoritePictures findByPictureId(Pictures pictureId);
 }
