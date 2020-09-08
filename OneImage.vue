9dd4b46ea47c293352e79c6d0a87d4c72eb8a29f
byungs2 <byungs2@naver.com>
2020-09-08 11:41:55 +0900

    2020-09-08 base64

---
+++ OneImage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id = "image_show_box">
+        <button type = "button" v-on:click = "req()">getPictures</button>
+        <div id = "show">
+            <img v-bind:src="imgSrc">
+        </div>
+    </div>
+</template>
+
+<script>
+const storage = window.sessionStorage;
+
+export default {
+    name : "OneImage",
+    data : function(){
+        return {
+            imgSrc : "",
+        }
+    },
+    methods: {
+        req : function(){
+            //let self = this
+            this.$axios.get("http://127.0.0.1:80/pictures/"+storage.getItem("userNumber"))
+            .then(res => {
+                console.log(res.data);
+            })
+        }
+    },
+}
+</script>
