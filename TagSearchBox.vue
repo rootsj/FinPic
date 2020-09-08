331b5f94a12d4ede84c06fe5249165b1f3e64cc3
rootsj <hwa9981@naver.com>
2020-09-08 14:54:51 +0900

    tagcheckbox 200908 update

--- TagSearchBox.vue
+++ TagSearchBox.vue
@@ -1,11 +1,12 @@
 <template>
   <div name="tagSearchBox">
-    <input type="text" v-bind:placeholder="tag" v-on:keyup.enter="search()"/>
+    <input type="text" v-bind:placeholder="tag" v-on:keyup.enter="search()" v-model= "tagName"/>
     <button
         type="button"
         v-on:click="search()"
-        v-bind:disabled="(tag.length == 0)">검색</button>
-    <div id = "show" v-for="(img, index) in imSrc" v-bind:key = "index">
+        v-bind:disabled="(tag.length == 0) || tag == '태그를 검색하세요'">검색</button>
+    <div v-bind:class="{ active: isActive }"> {{tag}} - {{count}}</div>
+    <div id = "show" v-for="(img, index) in imgSrc" v-bind:key = "index">
       <img v-bind:src="img">
     </div>
 	</div>
@@ -18,21 +19,32 @@
     data: function () {
       return {
         tag: "태그를 검색하세요",
-        imgSrc : "",
+        tagName : "",
+        imgSrc: "",
+        count: "",
+        isActive: false,
       };
     },
     methods: {
       search: function () {
         let self = this;
+        this.isActive = true;
         this.$axios
-          .get("http://localhost:80/pictures-and-tags/"+document.getElementById("tag"))
+          .get("http://localhost:80/pictures-and-tags/"+self.tagName)
           .then((res) => {
-            self.imgSrc = res.data
+            self.imgSrc = res.data;
+            self.count = res.data.length;
             console.log(res.data);
           })
           .catch();
       },
-
     },
+    created() {
+      let self = this
+      this.$axios.get("http://127.0.0.1:80/all-pictures")
+        .then(res => {
+        self.imgSrc = res.data
+        })
+    }
   };
 </script>
