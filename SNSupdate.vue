47128518ea19d7094df4aa8943ff609780bea9dd
jj <asd061@naver.com>
2020-09-09 10:47:03 +0900

    0909 SNS 정보 / SNS update 추가

---
+++ SNSupdate.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="SNSupdate">
+    <input type="text" id="introduction" v-bind:placeholder="introduction" />
+    <input type="text" id="snsLink1" v-bind:placeholder="snsLink1" />
+    <input type="text" id="snsLink2" v-bind:placeholder="snsLink2" />
+    <input type="text" id="snsLink3" v-bind:placeholder="snsLink3" />
+    <button type="button" v-on:click="Update">Update</button>
+  </div>
+</template>
+<script>
+const storage = window.sessionStorage;
+
+export default {
+  name: "SNSupdate",
+  data() {
+    return {
+      introduction: "introduction",
+      snsLink1: "SNSlink1",
+      snsLink2: "SNSlink2",
+      snsLink3: "SNSlink3",
+    };
+  },
+  methods: {
+    Update() {
+      this.$axios
+        .put(
+          "http://localhost:80/users/" +
+            storage.getItem("userNumber") +
+            "/profileUpdate",
+          {
+            params: {
+              introduction: document.getElementById("introduction").value,
+              snsLink1: document.getElementById("snsLink1").value,
+              snsLink2: document.getElementById("snsLink2").value,
+              snsLink3: document.getElementById("snsLink3").value,
+            },
+          }
+        )
+        .then(() => {
+          console.log(document.getElementById("introduction").value);
+          console.log(document.getElementById("snsLink1").value);
+          console.log("안녕하세요");
+          this.$router.push("/mypage");
+          this.$router.go("/");
+        })
+        .catch();
+    },
+  },
+};
+</script>
