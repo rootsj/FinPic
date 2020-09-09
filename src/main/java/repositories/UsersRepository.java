0acc6535718744352a4e7d6d6dc618a82ac83ca2
jj <asd061@naver.com>
2020-09-09 11:38:19 +0900

    prfile컨트롤러와 user레파지토리에 fidByUserNumer 추가

--- src/main/java/repositories/UsersRepository.java
+++ src/main/java/repositories/UsersRepository.java
@@ -7,6 +7,7 @@ import usersDTO.Users;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long>{
+	public Users findByUserNumber(long userNumber);
 	public Users findByUserEmail(String userEmail);
 	public Users findByUserEmailAndUserPassword(String userEmail, String userPassword);
 	public Users findByUserName(String userName);
