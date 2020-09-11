a104ca0578559407856e2329b1067f637e4232ee
rootsj <hwa9981@naver.com>
2020-09-11 14:49:04 +0900

    followlistbutton 200911

--- src/components/FollowListButton.vue
+++ src/components/FollowListButton.vue
@@ -4,7 +4,8 @@
             FollowList({{followCount}})
         </button>
         <div id = "show" v-for="follow in followList" v-bind:key = "follow.followId">
-            <button type = "button" v-on:click = "mypage(follow.userEmail, follow.followNumber)">
+            <button type = "button" v-on:click = "followPage(follow.followEmail, follow.followNumber)">
+                <img v-bind:src="follow.followImg">
                 {{follow.followName}} {{follow.followEmail}} {{follow.followNumber}}
             </button>
         </div>
@@ -33,20 +34,24 @@
                     .get("http://localhost:80/follow/follow/"+self.userNumber)
                     .then((res) => {
                         self.followList = [];
-                            for(let i = 0 ; i < res.data.length;i++){
+                            for(let i = 0 ; i < res.data.data.length;i++){
                                 self.followList.push({
                                 followId : i,
-                                followName: res.data[i].toUser.userName,
-                                followNumber: res.data[i].toUser.userNumber,
-                                followEmail: res.data[i].toUser.userEmail,
+                                followName: res.data.data[i].toUser.userName,
+                                followNumber: res.data.data[i].toUser.userNumber,
+                                followEmail: res.data.data[i].toUser.userEmail,
+                                followImg: res.data.img[i],
                                 })
                             }
+                            console.log(self.followList);
                     })
                     .catch();
             },
-            mypage : function(y,z){
+
+            followPage : function(y,z){
                 storage.setItem("otherUserEmail", y);
                 storage.setItem("otherUserNumber", z);
+
                 this.$router.push("/mypage");
                 this.$router.go("/");
             },
