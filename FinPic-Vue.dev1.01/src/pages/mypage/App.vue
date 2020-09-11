2b97a0481783c66947e2ac48d599b2b3c8d341a5
jj <asd061@naver.com>
2020-09-11 15:09:38 +0900

    Vue1.01 업데이트

--- FinPic-Vue.dev1.01/src/pages/mypage/App.vue
+++ FinPic-Vue.dev1.01/src/pages/mypage/App.vue
@@ -7,23 +7,24 @@
       <ShowProfileImg />
       <ShowUserEmail />
       <ShowUserInfo />
-      <button type="button" v-on:click="update()" v-if="myUserNumber == userNumber">SHOWUPDATEPAGE</button>
-      <UpdateSNSinfo v-if="toggleUpdate" />
-      <UpdateInfo v-if="toggleUpdate" />
-      <UpdateProfileImg v-if="toggleUpdate" />
-      <DelProfileImgBtn v-if="toggleUpdate" />
-      <DeleteID v-if="toggleUpdate" />
-      <ShowPost v-if="toggleMyPicture" />
-      <FollowButton />
+      <button type = "button" v-on:click = "update()" v-if = "myUserNumber == userNumber">SHOWUPDATEPAGE</button>
+      <UpdateSNSinfo v-if= "toggleUpdate"/>
+      <UpdateInfo v-if = "toggleUpdate"/>
+      <UpdateProfileImg v-if = "toggleUpdate"/>
+      <DelProfileImgBtn v-if = "toggleUpdate"/>
+      <DeleteID v-if = "toggleUpdate"/>
+      <MyPageUserImage v-if = "toggleMyPicture"/>
+      <FollowButton/>
+      <FollowListButton/>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 <script>
 const storage = window.sessionStorage;
 
-import MainPageButton from "../../components/MainPageButton.vue";
-import TagSearchBoxNoAll from "../../components/TagSearchBoxNoAll.vue";
+import MainPageButton from '../../components/MainPageButton.vue'
+import TagSearchBoxNoAll from '../../components/TagSearchBoxNoAll.vue'
 import ShowUserInfo from "../../components/ShowUserInfo.vue";
 import ShowUserEmail from "../../components/ShowUserEmail.vue";
 import UpdateProfileImg from "../../components/UpdateProfileImg.vue";
@@ -31,58 +32,61 @@ import UpdateSNSinfo from "../../components/UpdateSNSinfo.vue";
 import ShowProfileImg from "../../components/ShowProfileImg.vue";
 import DelProfileImgBtn from "../../components/DelProfileImgBtn.vue";
 import UpdateInfo from "../../components/UpdateInfo.vue";
-import ShowPost from "../../components/ShowPost.vue";
+import MyPageUserImage from "../../components/MyPageUserImage.vue";
 import DeleteID from "../../components/DeleteID.vue";
 import FollowButton from "../../components/FollowButton.vue";
+import FollowListButton from "../../components/FollowListButton.vue";
 
 export default {
-  name: "App",
-  data: function () {
-    return {
-      pictureNumber: "",
-      userEamil: "",
-      userNumber: "",
-      myUserNumber: "",
-      toggleUpdate: false,
-      toggleTagPicture: false,
-      toggleMyPicture: true,
-      toggleFavorite: false,
-      toggleFollowList: false,
-      toggleOnePicture: false,
-    };
-  },
-  methods: {
-    update: function () {
-      this.toggleUpdate = true;
-      this.toggleTagPicture = false;
-      this.toggleMyPicture = false;
-      this.toggleFavorite = false;
-      this.toggleFollowList = false;
-      this.toggleOnePicture = false;
+    name : "App",
+    data : function(){
+      return {
+          pictureNumber : "",
+          userEamil : "",
+          userNumber : "",
+          myUserNumber : "",
+          toggleUpdate : false,
+          toggleTagPicture : false,
+          toggleMyPicture : true,
+          toggleFavorite : false,
+          toggleFollowList : false,
+          toggleOnePicture : false,
+      }
     },
-  },
-  components: {
-    MainPageButton,
-    TagSearchBoxNoAll,
-    ShowUserInfo,
-    ShowUserEmail,
-    UpdateProfileImg,
-    UpdateSNSinfo,
-    ShowProfileImg,
-    DelProfileImgBtn,
-    UpdateInfo,
-    ShowPost,
-    DeleteID,
-    FollowButton,
-  },
-  created() {
-    this.pictureNumber = storage.getItem("PictureNumber");
-    this.userEmail = storage.getItem("otherUserEmail");
-    this.userNumber = storage.getItem("otherUserNumber");
-    this.myUserNumber = storage.getItem("userNumber");
-    console.log(this.userEmail);
-  },
-};
+    methods : {
+      update : function () {
+        this.toggleUpdate = true;
+        this.toggleTagPicture = false;
+        this.toggleMyPicture = false;
+        this.toggleFavorite = false;
+        this.toggleFollowList = false;
+        this.toggleOnePicture = false;
+      },
+
+    },
+    components : {
+      MainPageButton,
+      TagSearchBoxNoAll,
+      ShowUserInfo,
+      ShowUserEmail,
+      UpdateProfileImg,
+      UpdateSNSinfo,
+      ShowProfileImg,
+      DelProfileImgBtn,
+      UpdateInfo,
+      MyPageUserImage,
+      DeleteID,
+      FollowButton,
+      FollowListButton,
+    },
+    created() {
+      this.pictureNumber = storage.getItem("PictureNumber");
+      this.userEmail = storage.getItem("otherUserEmail");
+      this.userNumber = storage.getItem("otherUserNumber");
+      this.myUserNumber = storage.getItem("userNumber");
+      console.log(this.userEmail);
+    },
+}
 </script>
 <style>
 #app {
